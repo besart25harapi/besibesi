a9a15fbe078533b0c5601fe460ad5900e6b88b10
bharapi bharapi <bharapi@c1r10s7.tirana.42.school>
2026-04-10 10:27:30 +0200

    enes

--- ft_strrev.c
+++ ft_strrev.c
@@ -6,11 +6,43 @@
 /*   By: bharapi <bharapi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/10 04:45:17 by bharapi           #+#    #+#             */
-/*   Updated: 2026/04/10 04:45:54 by bharapi          ###   ########.fr       */
+/*   Updated: 2026/04/10 10:22:14 by bharapi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 char *ft_strrev(char *str)
 {
-    
+    int i;
+    int j;
+    char temp;
+    i = 0;
+    j = 0;
+    while(str[i])
+    {
+        i++;
+    }
+    i--;
+    while(i > j )
+    {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        j++;
+        i--;
+    }
+    i = 0;
+    while(str[i])
+    {
+    write(1, &str[i], 1);
+    i++;
+    }
+}
+
+int main(void){
+    char str[] = "asdf";
+    ft_strrev(str);
+    write(1, "\n", 1);
+    return 0;
 }
